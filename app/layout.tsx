d0c6069dd44b8960d20ea109a37b3bdcacf87083
Tung Pham <tung.42@gmail.com>
2025-11-24 19:34:24 +0700

    images

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,21 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Khóa học Lập trình miễn phí",
   description: "Học lập trình web từ cơ bản đến nâng cao hoàn toàn miễn phí",
+  openGraph: {
+    title: "Khóa học Lập trình miễn phí",
+    description: "Học lập trình web từ cơ bản đến nâng cao hoàn toàn miễn phí",
+    siteName: "Khóa học Lập trình miễn phí",
+    images: [
+      {
+        url: "/1200x630.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Khóa học Lập trình miễn phí",
+      },
+    ],
+    locale: "vi_VN",
+    type: "website",
+  },
 };
 
 const RootLayout = ({ children }: React.PropsWithChildren) => (
